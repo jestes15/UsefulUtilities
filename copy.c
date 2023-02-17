269fd627ad7349ececa6087b5998787a65569509
jestes36 <Joshua.Estes@gtri.gatech.edu>
2023-02-17 17:24:01 -0600

    Finished implementation of all options

--- copy.c
+++ copy.c
@@ -3,40 +3,76 @@
 // TODO: Add support for copying multiple files to a directory
 int main(int argc, char *argv[])
 {
-    u_int8_t index;
+    u_int8_t argument, index = 1;
 
-    for (index = 1; index < argc; ++index)
+    for (argument = 1; argument < argc; ++argument)
     {
-        if (argv[index][0] == '-')
+        if (argv[argument][0] == '-')
         {
-            switch (argv[index][1])
+            uint8_t len_of_arg = strlen(argv[argument]);
+            for (index = 1; index < len_of_arg; ++index)
             {
-            case 'h':
-                printf("Usage: copy [OPTION] SOURCE DEST\n");
-                printf("Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.\n\n");
-                printf("  -h\t\tDisplay this help and exit\n");
-                printf("  -u\t\tUse the user space copy method\n");
-                printf("  -k\t\tUse the kernel space copy method\n");
-                printf("  -f\t\tForce overwrite of DEST if it already exists\n");
-                printf("  -n\t\tDo not overwrite an existing file\n");
-                exit(SUCCESS);
-            case 'u':
-                if (CHECK_KERNEL_SPACE_COPY_BIT(mode) || CHECK_MMAP_COPY_BIT(mode)) {
-                    printf("ERROR: Illegal argument passed: %s", argv[index]);
+                switch (argv[argument][index])
+                {
+                case 'h':
+                    printf("Usage: copy [OPTION] SOURCE DEST\n");
+                    printf("Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.\n\n");
+                    printf("  -h\t\tDisplay this help and exit\n");
+                    printf("  -u\t\tUse the user space copy method\n");
+                    printf("  -k\t\tUse the kernel space copy method\n");
+                    printf("  -m\t\tUse the mmap copt method\n");
+                    printf("  -f\t\tForce overwrite of DEST if it already exists\n");
+                    printf("  -n\t\tDo not overwrite an existing file\n");
+                    exit(SUCCESS);
+                    break;
+                case 'u':
+                    if (CHECK_KERNEL_SPACE_COPY_BIT(mode) || CHECK_MMAP_COPY_BIT(mode))
+                    {
+                        printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                        printf("Can not use k or m options with option u\n");
+                        exit(ILLEGAL_OPTIONS_PASSED);
+                    }
+                    ENABLE_USER_SPACE_COPY(mode);
+                    break;
+                case 'k':
+                    if (CHECK_USER_SPACE_COPY_BIT(mode) || CHECK_MMAP_COPY_BIT(mode))
+                    {
+                        printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                        printf("Can not use u or m options with option k\n");
+                        exit(ILLEGAL_OPTIONS_PASSED);
+                    }
+                    ENABLE_KERNEL_SPACE_COPY(mode);
+                    break;
+                case 'm':
+                    if (CHECK_USER_SPACE_COPY_BIT(mode) || CHECK_KERNEL_SPACE_COPY_BIT(mode))
+                    {
+                        printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                        printf("Can not use u or k options with option m\n");
+                        exit(ILLEGAL_OPTIONS_PASSED);
+                    }
+                    ENABLE_MMAP_COPY(mode);
+                    break;
+                case 'f':
+                    if (CHECK_FILE_PROTECTION(mode)) {
+                        printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                        printf("Can not use option n with option m\n");
+                        exit(ILLEGAL_OPTIONS_PASSED);
+                    }
+                    ENABLE_FORCE_WRITE(mode);
+                    break;
+                case 'n':
+                    if (CHECK_FORCE_WRITE(mode)) {
+                        printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                        printf("Can not use option n with option m\n");
+                        exit(ILLEGAL_OPTIONS_PASSED);
+                    }
+                    ENABLE_FILE_PROTECTION(mode);
+                    break;
+                default:
+                    printf("ERROR: Illegal argument passed: %c\n", argv[argument][index]);
+                    printf("Correct usage is: copy [OPTION] SOURCE DEST\n");
+                    break;
                 }
-                break;
-            case 'k':
-                printf("Usage: copy [OPTION] SOURCE DEST\n");
-                break;
-            case 'f':
-                printf("Usage: copy [OPTION] SOURCE DEST\n");
-                break;
-            case 'n':
-                printf("Usage: copy [OPTION] SOURCE DEST\n");
-                break;
-            default:
-                printf("Usage: copy [OPTION] SOURCE DEST\n");
-                break;
             }
         }
     }
--- copy.h
+++ copy.h
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum file_operations
 {
@@ -20,7 +21,8 @@ enum file_operations
 enum return_codes
 {
     SUCCESS = 0,
-    FAILURE = 1,
+    ILLEGAL_OPTIONS_PASSED
+
 };
 
 /*
@@ -28,14 +30,20 @@ enum return_codes
 * 0x01 - copy using user space
 * 0x02 - copy using sendfile
 * 0x04 - copy using mmap
+* 0x08 - enable force write
+* 0x10 - enable file protection
 */
 
 u_int8_t mode = 0;
 
-#define ENABLE_USER_SPACE_COPY(x) x &= 0x01
-#define ENABLE_KERNEL_SPACE_COPY(x) x &= 0x02
-#define ENABLE_MMAP_COPY(x) x &= 0x04
+#define ENABLE_USER_SPACE_COPY(x) x |= 0x01
+#define ENABLE_KERNEL_SPACE_COPY(x) x |= 0x02
+#define ENABLE_MMAP_COPY(x) x |= 0x04
+#define ENABLE_FORCE_WRITE(x) x |= 0x08
+#define ENABLE_FILE_PROTECTION(x) x |= 0x10
 
-#define CHECK_USER_SPACE_COPY_BIT(x) x & 0x01
-#define CHECK_KERNEL_SPACE_COPY_BIT(x) x & 0x02
-#define CHECK_MMAP_COPY_BIT(x) x & 0x04
+#define CHECK_USER_SPACE_COPY_BIT(x) x && 0x01
+#define CHECK_KERNEL_SPACE_COPY_BIT(x) x && 0x02
+#define CHECK_MMAP_COPY_BIT(x) x && 0x04
+#define CHECK_FORCE_WRITE(x) x && 0x08
+#define CHECK_FILE_PROTECTION(x) x && 0x10
