70073f636f67895a2879dce0f93e27ea60a50042
Joshua Estes <Joshua.Estes@gtri.gatech.edu>
2023-03-01 21:37:10 -0600

    Fixed code

---
+++ UnionTesting/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <random>
+#include <chrono>
+
+// http://soundfile.sapp.org/doc/WaveFormat/
+union WAV_HEADER
+{
+    /* variable to put data in originally */
+    char data[44];
+
+    struct
+    {
+        /* RIFF Chunk Descriptor */
+        uint8_t RIFF[4];    // RIFF Header Magic header
+        uint32_t ChunkSize; // RIFF Chunk Size
+        uint8_t WAVE[4];    // WAVE Header
+
+        /* "fmt" sub-chunk */
+        uint8_t fmt[4];         // FMT header
+        uint32_t Subchunk1Size; // Size of the fmt chunk
+        uint16_t AudioFormat;   // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+        uint16_t NumOfChan;     // Number of channels 1=Mono 2=Sterio
+        uint32_t SamplesPerSec; // Sampling Frequency in Hz
+        uint32_t bytesPerSec;   // bytes per second
+        uint16_t blockAlign;    // 2=16-bit mono, 4=16-bit stereo
+        uint16_t bitsPerSample; // Number of bits per sample
+
+        /* "data" sub-chunk */
+        uint8_t Subchunk2ID[4]; // "data"  string
+        uint32_t Subchunk2Size; // Sampled data length
+    };
+};
+
+int main()
+{
+    WAV_HEADER wavHeader = {
+        .data = {
+            0x52, 0x49, 0x46, 0x46, 0x00, 0x09, 0x6b, 0xdc, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20,
+            0x00, 0x00, 0x00, 0x10, 0x00, 0x01, 0x00, 0x02, 0x00, 0x00, 0x2b, 0x11, 0x00, 0x00, 0xac, 0x44,
+            0x00, 0x04, 0x00, 0x10, 0x64, 0x61, 0x74, 0x61, 0x00, 0x09, 0x6b, 0xb8}};
+
+    std::cout << "RIFF header:\t\t\t" << wavHeader.RIFF[0] << wavHeader.RIFF[1] << wavHeader.RIFF[2] << wavHeader.RIFF[3] << std::endl;
+    std::cout << "WAVE header:\t\t\t" << wavHeader.WAVE[0] << wavHeader.WAVE[1] << wavHeader.WAVE[2] << wavHeader.WAVE[3] << std::endl;
+    std::cout << "FMT:\t\t\t\t" << wavHeader.fmt[0] << wavHeader.fmt[1] << wavHeader.fmt[2] << wavHeader.fmt[3] << std::endl;
+    std::cout << "Data size:\t\t\t" << wavHeader.ChunkSize << std::endl;
+
+    // Display the sampling Rate from the header
+    std::cout << "Sampling Rate:\t\t\t" << wavHeader.SamplesPerSec << " Hz" << std::endl;
+    std::cout << "Number of bits used:\t\t" << wavHeader.bitsPerSample << std::endl;
+    std::cout << "Number of channels:\t\t" << wavHeader.NumOfChan << std::endl;
+    std::cout << "Number of bytes per second:\t" << wavHeader.bytesPerSec << std::endl;
+    std::cout << "Data length:\t\t\t" << wavHeader.Subchunk2Size << std::endl;
+    std::cout << "Audio Format:\t\t\t" << wavHeader.AudioFormat << std::endl;
+
+    std::cout << "Block align:\t\t\t" << wavHeader.blockAlign << std::endl;
+    std::cout << "Data string:\t\t\t" << wavHeader.Subchunk2ID[0] << wavHeader.Subchunk2ID[1] << wavHeader.Subchunk2ID[2] << wavHeader.Subchunk2ID[3] << std::endl;
+}
