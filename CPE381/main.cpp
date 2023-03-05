0bd26cfce75869d8e6f95a913667a3010d243ebd
Joshua Estes <jne0010@uah.edu>
2023-03-04 20:21:11 -0600

    Added new things

--- CPE381/main.cpp
+++ CPE381/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     std::cout << "Input wav file: ";
     std::cin >> input;
 
+    // FILE *wavFile = fopen(input.c_str(), "r");
     auto wavFile_input = std::ifstream(input, std::ios::binary);
     if (!wavFile_input)
     {
@@ -32,7 +33,6 @@ int main(int argc, char *argv[])
     char data[44];
     std::size_t bytesRead = wavFile_input.read(data, headerSize).gcount();
     wavFile_output.write(data, sizeof(data));
-    std::cout << "Header Read " << bytesRead << " bytes." << std::endl;
 
     wavHeader = reinterpret_cast<WAV_HEADER &>(data);
 
@@ -42,38 +42,49 @@ int main(int argc, char *argv[])
         uint64_t numSamples = wavHeader.Subchunk2Size / bytesPerSample;
         static const uint16_t DUAL_CHANNEL_SIZE = bytesPerSample;
 
-        std::unique_ptr<int16_t[]> buffer = std::unique_ptr<int16_t[]>(new int16_t[DUAL_CHANNEL_SIZE]);
+        std::unique_ptr<int16_t[]> input = std::unique_ptr<int16_t[]>(new int16_t[DUAL_CHANNEL_SIZE]);
+        std::unique_ptr<int32_t[]> buffer = std::unique_ptr<int32_t[]>(new int32_t[DUAL_CHANNEL_SIZE]);
         std::unique_ptr<int16_t[]> result = std::unique_ptr<int16_t[]>(new int16_t[DUAL_CHANNEL_SIZE]);
 
         std::random_device device;
-
         std::uniform_real_distribution<> dist(0.0, std::nextafter(1.0, std::numeric_limits<float>::max()));
 
-        int16_t max_channel_1;
-        int16_t max_channel_2;
+        int32_t max_channel_1_pre_noise = -65000;
+        int32_t max_channel_2_pre_noise = -65000;
 
-        while (wavFile_input.read(reinterpret_cast<char*>(buffer.get()), 4)) 
+        int32_t max_channel_1_post_noise = -65000;
+        int32_t max_channel_2_post_noise = -65000;
+
+        while (wavFile_input.read(reinterpret_cast<char *>(input.get()), 4))
         {
+
+            max_channel_1_pre_noise = std::max(max_channel_1_pre_noise, abs(input[0]));
+            max_channel_2_pre_noise = std::max(max_channel_2_pre_noise, abs(input[1]));
+
             for (uint64_t index = 0; index < DUAL_CHANNEL_SIZE; ++index)
             {
                 auto temp = dist(device);
-                result[index] = buffer[index] + 300 * temp - 150;
+                buffer[index] = static_cast<int32_t>(buffer[index] + 300 * temp - 150);
+
+                if (buffer[index] > 0x7FFF)
+                    result[index] = 0x7FFF;
+                else if (buffer[index] < static_cast<int16_t>(0x8000))
+                    result[index] = static_cast<int16_t>(0x8000);
+                else
+                    result[index] = static_cast<int16_t>(buffer[index]);
             }
 
-            if (abs(result[0]) > max_channel_1) {
-                max_channel_1 = abs(result[0]);
-            }
-            max_channel_1 = std::max(max_channel_1, static_cast<int16_t>(abs(result[0])));
-            max_channel_2 = std::max(max_channel_2, static_cast<int16_t>(abs(result[1])));
+            max_channel_1_post_noise = std::max(max_channel_1_post_noise, static_cast<int32_t>(result[0]));
+            max_channel_2_post_noise = std::max(max_channel_2_post_noise, static_cast<int32_t>(result[1]));
 
             wavFile_output.write(reinterpret_cast<const char *>(result.get()), sizeof(uint32_t));
         }
 
         auto stop_time = std::chrono::high_resolution_clock::now();
         print_data(wavHeader, std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count(), numSamples);
-        print_summary_text_file(wavHeader, start_time, stop_time, numSamples, max_channel_1, max_channel_2);
+        print_summary_text_file(wavHeader, start_time, stop_time, numSamples, max_channel_1_pre_noise, max_channel_2_pre_noise, max_channel_1_post_noise, max_channel_2_post_noise);
     }
-    
+
     wavFile_input.close();
     wavFile_output.close();
     return 0;
--- CPE381/wav_file_parser.hpp
+++ CPE381/wav_file_parser.hpp
@@ -1,36 +1,39 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <fstream>
 #include <cstdint>
 #include <random>
 #include <chrono>
+#include <memory>
 
 // http://soundfile.sapp.org/doc/WaveFormat/
 struct WAV_HEADER
 {
-    /* RIFF Chunk Descriptor */
-    uint8_t     RIFF[4];    // RIFF Header Magic header
-    uint32_t    ChunkSize; // RIFF Chunk Size
-    uint8_t     WAVE[4];    // WAVE Header
+        /* RIFF Chunk Descriptor */
+        uint8_t RIFF[4];    // RIFF Header Magic header
+        uint32_t ChunkSize; // RIFF Chunk Size
+        uint8_t WAVE[4];    // WAVE Header
 
-    /* "fmt" sub-chunk */
-    uint8_t     fmt[4];         // FMT header
-    uint32_t    Subchunk1Size; // Size of the fmt chunk
-    uint16_t    AudioFormat;   // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-    uint16_t    NumOfChan;     // Number of channels 1=Mono 2=Sterio
-    uint32_t    SamplesPerSec; // Sampling Frequency in Hz
-    uint32_t    bytesPerSec;   // bytes per second
-    uint16_t    blockAlign;    // 2=16-bit mono, 4=16-bit stereo
-    uint16_t    bitsPerSample; // Number of bits per sample
+        /* "fmt" sub-chunk */
+        uint8_t fmt[4];         // FMT header
+        uint32_t Subchunk1Size; // Size of the fmt chunk
+        uint16_t AudioFormat;   // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+        uint16_t NumOfChan;     // Number of channels 1=Mono 2=Sterio
+        uint32_t SamplesPerSec; // Sampling Frequency in Hz
+        uint32_t bytesPerSec;   // bytes per second
+        uint16_t blockAlign;    // 2=16-bit mono, 4=16-bit stereo
+        uint16_t bitsPerSample; // Number of bits per sample
 
-    /* "data" sub-chunk */
-    uint8_t     Subchunk2ID[4]; // "data"  string
-    uint32_t    Subchunk2Size; // Sampled data length
+        /* "data" sub-chunk */
+        uint8_t Subchunk2ID[4]; // "data"  string
+        uint32_t Subchunk2Size; // Sampled data length
 };
 
-
-void print_data(WAV_HEADER &wavHeader, uint_fast64_t time_delta, uint64_t numSamples) {
-    std::cout << wavHeader.RIFF << std::endl;
+void print_data(WAV_HEADER &wavHeader,
+                uint_fast64_t time_delta,
+                uint64_t numSamples)
+{
         std::cout << "RIFF header:\t\t\t" << wavHeader.RIFF[0] << wavHeader.RIFF[1] << wavHeader.RIFF[2] << wavHeader.RIFF[3] << std::endl;
         std::cout << "WAVE header:\t\t\t" << wavHeader.WAVE[0] << wavHeader.WAVE[1] << wavHeader.WAVE[2] << wavHeader.WAVE[3] << std::endl;
         std::cout << "FMT:\t\t\t\t" << wavHeader.fmt[0] << wavHeader.fmt[1] << wavHeader.fmt[2] << wavHeader.fmt[3] << std::endl;
@@ -52,16 +55,28 @@ void print_data(WAV_HEADER &wavHeader, uint_fast64_t time_delta, uint64_t numSam
         std::cout << "Seconds: " << (numSamples / 2) / wavHeader.SamplesPerSec << " s" << std::endl;
 }
 
-void print_summary_text_file(WAV_HEADER &wavHeader, std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point stop_time, uint64_t numSamples, int16_t max_channel_1, int16_t max_channel_2) {
-    std::ofstream test_file("Estes_J_sim.txt", std::ios::out);
-    test_file << "Name:\t\t\t\t\t\tJoshua Estes" << std::endl;
-    test_file << "File Name:\t\t\t\t\tEstes_J_sim.txt" << std::endl;
-    test_file << "Number of samples:\t\t\t" << numSamples << std::endl;
-    test_file << "Sampling Frequency:\t\t\t" << wavHeader.ChunkSize << std::endl;
-    test_file << "Number of bits per sample:\t" << wavHeader.bitsPerSample << std::endl;
-    test_file << "Record length:\t\t\t\t" << numSamples / (wavHeader.NumOfChan * wavHeader.SamplesPerSec) << std::endl;
-    test_file << "Maximum absolute value in channel 1:\t" << abs(max_channel_1) << std::endl;
-    test_file << "Maximum absolute value in channel 2:\t" << abs(max_channel_2) << std::endl;
-    test_file << "Execution time:\t\t\t\t" << std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count() << " nanoseconds" << std::endl;
-    test_file << "Execution time:\t\t\t\t" << std::chrono::duration_cast<std::chrono::milliseconds>(stop_time - start_time).count() << " milliseconds" << std::endl;
+void print_summary_text_file(WAV_HEADER &wavHeader,
+                             std::chrono::system_clock::time_point start_time,
+                             std::chrono::system_clock::time_point stop_time,
+                             uint64_t numSamples,
+                             int32_t max_channel_1_pre_noise,
+                             int32_t max_channel_2_pre_noise,
+                             int32_t max_channel_1_post_noise,
+                             int32_t max_channel_2_post_noise)
+{
+        std::ofstream test_file("Estes_J_sim.txt", std::ios::out);
+        test_file << std::left << std::setw(50) << "Name:"
+                  << "Joshua Estes" << std::endl;
+        test_file << std::left << std::setw(50) << "File Name:"
+                  << "Estes_J_sim.txt" << std::endl;
+        test_file << std::left << std::setw(50) << "Number of samples:" << numSamples << std::endl;
+        test_file << std::left << std::setw(50) << "Sampling Frequency:" << wavHeader.SamplesPerSec << std::endl;
+        test_file << std::left << std::setw(50) << "Number of bits per sample:" << wavHeader.bitsPerSample << std::endl;
+        test_file << std::left << std::setw(50) << "Record length:" << numSamples / (wavHeader.NumOfChan * wavHeader.SamplesPerSec) << std::endl;
+        test_file << std::left << std::setw(50) << "Maximum value in channel 1 before noise:" << max_channel_1_pre_noise << std::endl;
+        test_file << std::left << std::setw(50) << "Maximum value in channel 2 before noise:" << max_channel_2_pre_noise << std::endl;
+        test_file << std::left << std::setw(50) << "Maximum value in channel 1 after noise:" << max_channel_1_post_noise << std::endl;
+        test_file << std::left << std::setw(50) << "Maximum value in channel 2 after noise:" << max_channel_2_post_noise << std::endl;
+        test_file << std::left << std::setw(50) << "Execution time in nanoseconds:" << std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count() << " nanoseconds" << std::endl;
+        test_file << std::left << std::setw(50) << "Execution time in milliseconds:" << std::chrono::duration_cast<std::chrono::milliseconds>(stop_time - start_time).count() << " milliseconds";
 }
